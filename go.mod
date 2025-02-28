f78bd475b25dec388208192d615f89a4cf54329b
Nano7795 <hatorkihalabrahma@gmail.com>
2025-02-28 11:36:43 +0530

    initial commint

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Nano7795/kalua
+
+go 1.24.0
---
+++ kalua.go
@@ -0,0 +1,8 @@
+package kalari
+
+import "fmt"
+
+// Greet function returns a greeting message
+func Greet(name string) string {
+    return fmt.Sprintf("Hello, %s!", name)
+}
